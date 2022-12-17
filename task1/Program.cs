5bf9e38f07f055e1f6eb399db073aaedcbd83d23
Amiko8 <119101599+Amiko8@users.noreply.github.com>
2022-12-17 17:20:48 +0300

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Количество чисел больше нуля {count}");
